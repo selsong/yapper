ea2f763d41a6d42d791e1a47e90c13961ae56da4
Selina Song <selinasong6@gmail.com>
2025-03-03 23:11:42 -0800

    Initial Commit

---
+++ yapper/yapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  yapperApp.swift
+//  yapper
+//
+//  Created by Selina Song on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct yapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
